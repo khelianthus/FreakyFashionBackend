ffd01ad6372738fe3b01c8f05bc8364ca5a5e95a
Karoline Aspegren <karolineaspegren@gmail.com>
2023-05-29 17:52:37 +0200

    Add GetProducts and ToProductDto to ProductsController

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using FreakyFashion.Data;
+using FreakyFashion.Models.Domain;
 using FreakyFashion.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +27,8 @@ namespace FreakyFashion.Controllers
                 return NotFound();
             }
 
+            // Nedan kan ändras till ->       var productDto = ToProductDto(product);
+
             var productDto = new ProductDto
             {
                 Id = product.Id,
@@ -44,5 +47,34 @@ namespace FreakyFashion.Controllers
 
             return productDto;
         }
+
+        [HttpGet]
+        public IEnumerable<ProductDto> GetProducts([FromQuery] string? name)
+        {
+            var products = name is null
+               ? context.Products.ToList()
+               : context.Products.Where(x => x.Name.Contains(name)).ToList();
+
+            var productDtos = products.Select(ToProductDto);
+
+            return productDtos;
+        }
+
+        private ProductDto ToProductDto(Product product)
+           => new ProductDto
+           {
+               Id = product.Id,
+               Name = product.Name,
+               Description = product.Description,
+               Brand = product.Brand,
+               Price = product.Price,
+               Sku = product.Sku,
+               ImageUrl = product.ImageUrl,
+               UrlSlug = product.UrlSlug,
+               Likes = product.Likes,
+               Color = product.Color,
+               Category = product.Category,
+               CreatedAt = product.CreatedAt
+           };
     }
-}
+ }
