c3d9a9429b715bbcf76d04ee8be1b153f0fbca40
Fredde88 <tuffing_88@hotmail.com>
2023-06-01 15:56:02 +0200

    Add new info in seed-data and reverted GetProducts function

--- Data/seed-data.sql
+++ Data/seed-data.sql
@@ -12,8 +12,9 @@ INSERT INTO Products (
    CategoryId
 )
 VALUES
-('T-Shirt', 'Lorem ipsum dolor sit amet', 'Levis', 199, 'AAA111', 'https://via.placeholder.com/320x480.png', 'svart-tshirt', 180, 'Svart', '2023-05-29', 2),
-('T-Shirt', 'Lorem ipsum dolor sit amet', 'Nike', 229, 'BBB111', 'https://via.placeholder.com/320x480.png', 'vit-tshirt', 180, 'Vit', '2023-05-29', 2)
+('T-Shirt', 'Lorem ipsum dolor sit amet', 'Levis', 199, 'AAA111', 'https://via.placeholder.com/320x480.png?text=Svart+T-shirt', 'svart-tshirt', 180, 'Svart', '2023-05-29', 2),
+('T-Shirt', 'Lorem ipsum dolor sit amet', 'Nike', 229, 'BBB111', 'https://via.placeholder.com/320x480.png?text=Vit+T-shirt', 'vit-tshirt', 180, 'Vit', '2023-05-29', 2)
+('T-Shirt', 'Lorem ipsum dolor sit amet', 'Nike', 229, 'AAA222', 'https://via.placeholder.com/320x480.png?text=Rosa+T-shirt', 'rosa-tshirt', 180, 'Rosa', '2023-05-29', 2)
 
 INSERT INTO Categories (
 	Name
