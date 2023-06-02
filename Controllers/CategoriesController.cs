f75d4e49018158e2b5f4fd469836a82eca5686ce
Linnea <linnea.t.andersson@gmail.com>
2023-06-02 11:08:13 +0200

    Add CategoryDto and CategoriesController

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using FreakyFashion.Data;
+using FreakyFashion.Models.Domain;
+using FreakyFashion.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashion.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly ApplicationDbContext context;
+
+    public CategoriesController(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<CategoryDto> GetCategories()
+    {
+        var categories = context.Categories
+                .Include(x => x.Products)
+                .ToList();
+
+        var categoryDtos = categories.Select(ToCategoryDto);
+
+        return categoryDtos;
+    }
+
+    private CategoryDto ToCategoryDto(Category category)
+          => new CategoryDto
+          {
+              Id = category.Id,
+              Name = category.Name,
+              Products = category.Products
+          };
+}
---
+++ Models/DTO/CategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using FreakyFashion.Models.Domain;
+
+namespace FreakyFashion.Models.DTO
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
